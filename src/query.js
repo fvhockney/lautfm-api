56c7b45d51d1c2670e1cfcbedf382f2f7ada40bc
Vern Hockney <vern@laut.fm>
2019-08-16 14:04:03 +0200

    initial commit docs

---
+++ src/query.js
@@ -0,0 +1,23 @@
+import axios from 'axios'
+
+export const query = async function(url, params = {}) {
+    const responseType = 'json'
+    const timeout = 2000
+    const baseURL = 'https://api.laut.fm/'
+    try {
+        const response = await axios({
+            method: 'get',
+            baseURL,
+            url,
+            responseType,
+            params,
+            timeout,
+        })
+        const data = await response.data
+        return data
+    } catch (err) {
+        console.log(err)
+        throw err
+    }
+}
+
